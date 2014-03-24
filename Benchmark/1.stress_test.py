bee922fb5723ace97d9ddfd18e8b37ed0ec1b340
jsvalenca <jsvalenca@gmail.com>
2014-03-24 16:48:08 +0000

    testes e cenas

--- Benchmark/1.stress_test.py
+++ Benchmark/1.stress_test.py
@@ -4,30 +4,40 @@ import time
 import MySQLdb
 
 def val(v_id):
-	db = MySQLdb.connect(host="localhost",user="root",passwd="password",db="tms")
+	db = MySQLdb.connect(host="localhost",user="root",passwd="",db="tms")
 	cursor = db.cursor()
-	with gzip.open('Validations/val_'+str(v_id)+'.gz.db', 'rb') as f:
-		for line in f:
-			duration = time.time()
-			cursor.execute(line)
-			db.commit()
-			print time.time() - duration
+	n=str(v_id)
+	i=0
+	with gzip.open('Validations/val_'+n+'.gz.db', 'rb') as f:
+		with open('valtime'+n+'.db','w') as g:
+			for line in f:
+				duration = time.time()
+				cursor.execute(line)
+				db.commit()
+				g.write(time.time() - duration)
+				print "V",i
+				i+=1
 
 def dep(d_id):
-	db = MySQLdb.connect(host="localhost",user="root",passwd="password",db="tms")
+	db = MySQLdb.connect(host="localhost",user="root",passwd="",db="tms")
 	cursor = db.cursor()
-	with gzip.open('Deposits/dep_'+str(d_id)+'.gz.db', 'rb') as f:
-		for line in f:
-			duration = time.time()
-			cursor.execute(line)
-			db.commit()
-			print time.time() - duration
+	n=str(d_id)
+	i=0
+	with gzip.open('Deposits/dep_'+n+'.gz.db', 'rb') as f:
+		with open('deptime'+n+'.db','w') as g:
+			for line in f:
+				duration = time.time()
+				cursor.execute(line)
+				db.commit()
+				g.write(time.time() - duration)
+				print "D",i
+				i+=1
 
-#validations = [Process(target=val, args=(i,)) for i in range(10)]
-#deposits = [Process(target=dep, args=(i,)) for i in range(10)]
+validations = [Process(target=val, args=(i,)) for i in range(10)]
+deposits = [Process(target=dep, args=(i,)) for i in range(10)]
 
-#[i.start() for i in validations]
-#[i.start() for i in deposits]
+[i.start() for i in validations]
+[i.start() for i in deposits]
 
-#[i.join() for i in validations]
-#[i.join() for i in deposits]
+[i.join() for i in validations]
+[i.join() for i in deposits]
