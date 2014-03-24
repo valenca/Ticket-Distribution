48bfdae04abcac1560e0afe1025ced38e5a79418
jsvalenca <jsvalenca@gmail.com>
2014-03-24 18:02:55 +0000

    more tests

--- Benchmark/1.stress_test.py
+++ Benchmark/1.stress_test.py
@@ -5,7 +5,7 @@ import MySQLdb
 
 def val(v_id):
 	i = 0
-	db = MySQLdb.connect(host="localhost",user="root",passwd="password",db="tms")
+	db = MySQLdb.connect(host="localhost",user="root",passwd="",db="tms")
 	cursor = db.cursor()
 	with gzip.open('Validations/val_'+v_id+'.gz.db', 'rb') as f:
 		with open('Validations/time_'+v_id+'.loc','w') as g:
@@ -19,7 +19,7 @@ def val(v_id):
 
 def dep(d_id):
 	i = 0
-	db = MySQLdb.connect(host="localhost",user="root",passwd="password",db="tms")
+	db = MySQLdb.connect(host="localhost",user="root",passwd="",db="tms")
 	cursor = db.cursor()
 	with gzip.open('Deposits/dep_'+str(d_id)+'.gz.db', 'rb') as f:
 		with open('Deposits/time_'+d_id+'.loc','w') as g:
@@ -34,10 +34,10 @@ def dep(d_id):
 				
 
 validations = [Process(target=val, args=(str(i),)) for i in range(10)]
-#deposits = [Process(target=dep, args=(str(i),)) for i in range(10)]
+deposits = [Process(target=dep, args=(str(i),)) for i in range(10)]
 
 [i.start() for i in validations]
-#[i.start() for i in deposits]
+[i.start() for i in deposits]
 
 [i.join() for i in validations]
-#[i.join() for i in deposits]
+[i.join() for i in deposits]
