0fbe53883128cdb73478fcc28f78de9408f2f503
Gustavo Martins <gus.a.martins@gmail.com>
2014-03-27 08:11:10 +0000

    new creator done

--- Benchmark/1.stress_test.py
+++ Benchmark/1.stress_test.py
@@ -32,16 +32,15 @@ def dep(d_id):
 				cursor.execute(line)
 				db.commit()
 				g.write(str(time.time() - duration)+'\n')
-				if i % 1000 == 0:
-					print 'Dep_'+d_id+": "+str(i)
+				print 'Dep_'+d_id+": "+str(i)
 			g.write(str(time.time() - duration2)+'\n')
 				
 
 validations = [Process(target=val, args=(str(i),)) for i in range(10)]
 deposits = [Process(target=dep, args=(str(i),)) for i in range(10)]
 
-[i.start() for i in deposits]
 [i.start() for i in validations]
+[i.start() for i in deposits]
 
-[i.join() for i in deposits]
 [i.join() for i in validations]
+[i.join() for i in deposits]
