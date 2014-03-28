7d8ca1a64ed68f2e45667afba2f2d6a3e78fe420
valenca <jsvalenca@gmail.com>
2014-03-28 15:00:04 +0000

    changes

--- Benchmark/1.stress_test.py
+++ Benchmark/1.stress_test.py
@@ -4,7 +4,7 @@ import time
 import MySQLdb
 
 def val(v_id):
-	db = MySQLdb.connect(host="localhost",user="root",passwd="password",db="tms")
+	db = MySQLdb.connect(host="localhost",user="root",passwd="",db="tms")
 	cursor = db.cursor()
 	with gzip.open('Validations/val_'+str(v_id)+'.gz.db', 'rb') as f:
 		for line in f:
@@ -14,7 +14,7 @@ def val(v_id):
 			print time.time() - duration
 
 def dep(d_id):
-	db = MySQLdb.connect(host="localhost",user="root",passwd="password",db="tms")
+	db = MySQLdb.connect(host="localhost",user="root",passwd="",db="tms")
 	cursor = db.cursor()
 	with gzip.open('Deposits/dep_'+str(d_id)+'.gz.db', 'rb') as f:
 		for line in f:
@@ -23,11 +23,11 @@ def dep(d_id):
 			db.commit()
 			print time.time() - duration
 
-#validations = [Process(target=val, args=(i,)) for i in range(10)]
+validations = [Process(target=val, args=(i,)) for i in range(10)]
 #deposits = [Process(target=dep, args=(i,)) for i in range(10)]
 
-#[i.start() for i in validations]
+[i.start() for i in validations]
 #[i.start() for i in deposits]
 
-#[i.join() for i in validations]
+[i.join() for i in validations]
 #[i.join() for i in deposits]
