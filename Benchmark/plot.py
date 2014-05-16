aaf1849eca5596449d997f69ecb7026d537141cd
Gustavo Martins <gus.a.martins@gmail.com>
2014-05-16 17:27:38 +0100

    plots

--- Benchmark/plot.py
+++ Benchmark/plot.py
@@ -4,15 +4,20 @@ import os
 
 py = plotly.plotly("Mehlins", "vq0aqwkoz4")
 
-with open('revenue.loc') as f:
+with open('Results/Standalone/revenue.loc') as f:
 	data1 = [float(f.readline().split()[1]) for i in range(100000)]
 data1.sort()
 data1 = [sum(data1[j*1000:(j+1)*1000])/1000 for j in range(100)]
 
-with open('revenue2.loc') as f:
+with open('Results/Standalone/revenue2.loc') as f:
 	data2 = [float(f.readline().split()[1]) for i in range(100000)]
 data2.sort()
 data2 = [sum(data2[j*1000:(j+1)*1000])/1000 for j in range(100)]
+
+with open('Results/Cluster/revenue.loc') as f:
+	data3 = [float(f.readline().split()[1]) for i in range(100000)]
+data3.sort()
+data3 = [sum(data3[j*1000:(j+1)*1000])/1000 for j in range(100)]
 
 box1 = {'y': data1,
   	'type': 'box',
@@ -26,6 +31,12 @@ box2 = {'y': data2,
   	'jitter': 0.1,
   	'pointpos': -1.2}
 
-py.plot([box1,box2])
+box3 = {'y': data3,
+  	'type': 'box',
+  	'boxpoints': 'all',
+  	'jitter': 0.1,
+  	'pointpos': -1.2}
+
+py.plot([box1,box2,box3])
 
 
--- Generation/plot.py
+++ Generation/plot.py
@@ -4,13 +4,12 @@ import os
 
 py = plotly.plotly("Mehlins", "vq0aqwkoz4")
 
-indexes_d = list(range(1,101,5))
-indexes_v = list(range(1,21))
+indexes = list(range(1,21))
 
 data_d1 = []
 total_d1 = []
-for f in [i for i in os.listdir('Deposits/') if i[:4] == 'time']:
-	with open('Deposits/'+f) as g:
+for f in [i for i in os.listdir('Results/Standalone/Deposits/') if i[:4] == 'time']:
+	with open('Results/Standalone/Deposits/'+f) as g:
 		data_d1.append([float(g.readline()) for j in range(100)])
 		total_d1.append(float(g.readline()))
 data_d1 = [float(sum(l))/len(l) for l in zip(*data_d1)]
@@ -18,43 +17,65 @@ data_d1 = [sum(data_d1[j*5:(j+1)*5])/5 for j in range(20)]
 
 data_d2 = []
 total_d2 = []
-for f in [i for i in os.listdir('Deposits2/') if i[:4] == 'time']:
-	with open('Deposits2/'+f) as g:
+for f in [i for i in os.listdir('Results/Standalone/Deposits2/') if i[:4] == 'time']:
+	with open('Results/Standalone/Deposits2/'+f) as g:
 		data_d2.append([float(g.readline()) for j in range(100)])
 		total_d2.append(float(g.readline()))
 data_d2 = [float(sum(l))/len(l) for l in zip(*data_d2)]
 data_d2 = [sum(data_d2[j*5:(j+1)*5])/5 for j in range(20)]
 
+data_d3 = []
+total_d3 = []
+for f in [i for i in os.listdir('Results/Cluster/Deposits/') if i[:4] == 'time']:
+	with open('Results/Cluster/Deposits/'+f) as g:
+		data_d3.append([2*float(g.readline()) for j in range(100)])
+		total_d3.append(2*float(g.readline()))
+data_d3 = [float(sum(l))/len(l) for l in zip(*data_d3)]
+data_d3 = [sum(data_d3[j*5:(j+1)*5])/5 for j in range(20)]
+
 data_v1 = []
 total_v1 = []
-for f in [i for i in os.listdir('Validations/') if i[:4] == 'time']:
-	with open('Validations/'+f) as g:
+for f in [i for i in os.listdir('Results/Standalone/Validations/') if i[:4] == 'time']:
+	with open('Results/Standalone/Validations/'+f) as g:
 		data_v1.append([float(g.readline()) for j in range(20)])
 		total_v1.append(float(g.readline()))
 data_v1 = [float(sum(l))/len(l) for l in zip(*data_v1)]
 
 data_v2 = []
 total_v2 = []
-for f in [i for i in os.listdir('Validations2/') if i[:4] == 'time']:
-	with open('Validations2/'+f) as g:
+for f in [i for i in os.listdir('Results/Standalone/Validations2/') if i[:4] == 'time']:
+	with open('Results/Standalone/Validations2/'+f) as g:
 		data_v2.append([float(g.readline()) for j in range(20)])
 		total_v2.append(float(g.readline()))
 data_v2 = [float(sum(l))/len(l) for l in zip(*data_v2)]
 
+data_v3 = []
+total_v3 = []
+for f in [i for i in os.listdir('Results/Cluster/Validations/') if i[:4] == 'time']:
+	with open('Results/Cluster/Validations/'+f) as g:
+		data_v3.append([5*float(g.readline()) for j in range(100)])
+		total_v3.append(5*float(g.readline()))
+data_v3 = [float(sum(l))/len(l) for l in zip(*data_v3)]
+data_v3 = [sum(data_v3[j*5:(j+1)*5]) for j in range(20)]
 
-trace0 = {'x': indexes_d,
+
+trace0 = {'x': indexes,
   'y': data_d1}
 
-trace1 = {'x': indexes_d,
+trace1 = {'x': indexes,
   'y': data_d2}
 
-trace2 = {'x': indexes_v,
+trace2 = {'x': indexes,
+  'y': data_d3}
+
+trace3 = {'x': indexes,
   'y': data_v1}
 
-trace3 = {'x': indexes_v,
+trace4 = {'x': indexes,
   'y': data_v2}
 
-py.plot([trace0, trace1])
-py.plot([trace2, trace3])
+trace5 = {'x': indexes,
+  'y': data_v3}
 
-
+py.plot([trace0, trace1, trace2])
+py.plot([trace3, trace4, trace5])
